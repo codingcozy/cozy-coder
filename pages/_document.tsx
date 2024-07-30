c949059c6146fbff98a3d9e4415cfe00893119ab
codingcozy <codingcozy@gmail.com>
2024-07-30 15:29:12 +0900

    posting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TK5CQ2GCKX"></script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TK5CQ2GCKX"></Script>
         <script
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
